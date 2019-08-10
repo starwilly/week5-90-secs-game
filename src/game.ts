6fb3174297ceb1e9d90c41f43a44fdf4318e7740
will <willystar@gmail.com>
2019-08-10 23:59:24 +0800

    setup moving and frame

--- src/game.ts
+++ src/game.ts
@@ -5,10 +5,16 @@ type GameConfig = Phaser.Types.Core.GameConfig;
 
 export const config: GameConfig = {
     title: "90 Sec Game",
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
     backgroundColor: '#ccc',
-    scene: [GameScene]
+    scene: [GameScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            // debug: true
+        }
+    }
 };
 
 export class MyGame extends Phaser.Game {
--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -1,22 +1,61 @@
 import * as Phaser from 'phaser';
+import { config } from '../game';
 
-import BgImage from './../assets/images/bg.svg';
+import BgPng from '../assets/images/bg.png';
+import DuckSprite from '../assets/images/duck-sprite.svg';
+import FrameTop from '../assets/images/frame-top.svg';
+import FrameBottom from '../assets/images/frame-bottom.svg';
 
 export class GameScene extends Phaser.Scene {
 
     private bg: Phaser.GameObjects.TileSprite;
+    private duck: Phaser.GameObjects.Sprite;
+    private map = {
+        width: 1200,
+        height: 600000
+    };
 
     preload(): void {
-        this.load.image('bg', BgImage)
+        this.load.image('bg', BgPng);
+        this.load.spritesheet('duck', DuckSprite, {frameWidth: 130, frameHeight: 230});
+        this.load.image('frameTop', FrameTop);
+        this.load.image('frameBottom', FrameBottom);
+    }
+
+    setUpFrame(): void {
+        this.bg = this.add.tileSprite(0, 0, 0, 0, 'bg').setOrigin(0, 0).setScrollFactor(0, 0);
+        const frameHeight = 121;
+        const graphics = this.add.graphics({fillStyle: {color: 0xcccccc}});
+        // draw a react to overlay tile
+        const rect = new Phaser.Geom.Rectangle(0, this.map.height - frameHeight, <number>config.width, frameHeight);
+        graphics.fillRectShape(rect);
+        this.add.image(0, this.map.height - frameHeight, 'frameBottom').setOrigin(0, 0).setScrollFactor(0, 1);
     }
 
     create(): void {
-        console.log('create');
-        this.bg = this.add.tileSprite(<number>this.game.config.width / 2,
-            <number>this.game.config.height / 2, 1200, 900, 'bg');
+        // Bound
+        this.cameras.main.setBounds(0, 0, this.map.width, this.map.height);
+        this.physics.world.setBounds(0, 0, this.map.width, this.map.height);
+
+        const stageWidth: number = this.game.config.width as number;
+        const stageHeight: number = this.game.config.height as number;
+        this.setUpFrame();
+
+        this.duck = this.physics.add.sprite(stageWidth / 2, this.map.height - 200, 'duck');
+        this.anims.create({
+            key: 'move',
+            frames: this.anims.generateFrameNumbers('duck', {start: 0, end: 3}),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.duck.anims.play('move');
+        this.cameras.main.startFollow(this.duck, true, 1, 1, 0, 320);
     }
 
     update(): void {
-        this.bg.tilePositionY -= 1;
+        const speed = 2;
+        this.bg.tilePositionY -= speed;
+        this.duck.y -= speed;
+        console.log(this.duck.y);
     }
 }
