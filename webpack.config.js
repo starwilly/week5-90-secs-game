99c7f3c9e1ce4a78441a83f20f046bb7ee5c8bdc
will <willystar@gmail.com>
2019-08-10 16:49:20 +0800

    Add file-loader and background image

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,12 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
