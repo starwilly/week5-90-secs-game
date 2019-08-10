3f7c82e424cc6f04d850013f1adf7a9b14785f02
will <willystar@gmail.com>
2019-08-10 14:15:13 +0800

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
